6e0d630e6c6487e8316f2dc6b816efb9b3161117
ThiagoZ <zanetet29@gmail.com>
2025-10-28 12:24:25 -0300

    Implementando uma função de calcular soma

--- include/bib.hpp
+++ include/bib.hpp
@@ -2,5 +2,6 @@
 #define BIB_HPP
 
 int calcularFatorial(int n);
+int calcularSoma(int n1, int n2);
 
 #endif
--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <cassert>
 #include "../include/bib.hpp"
 using namespace std;
 
 int main() {
     int n = 5;
     int resultado = calcularFatorial(n);
-    if (resultado == 120) {
-        cout << "Teste OK" << endl;
-        return 0;
-    } else {
-        cout << "Teste falhou: resultado = " << resultado << endl;
-        return 1;
-    }
+    assert(resultado == 120);
+    int soma = calcularSoma(2, 2);
+    assert(soma == 4);
+    
 }
+/*
+git checkout develop
+git checkout -b feat/calcularFatorial
+make
+make test
+./bin/testeRegressivo.exe
+git add .
+git commit -m "Implementa função calcularFatorial e testes"
+git checkout develop
+git merge feat/calcularFatorial
+git push origin develop*/
